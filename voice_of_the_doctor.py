ea1bdd61feca5f1af567f5d718b0bf57489f3f63
Souvik Chatterjee <souvikchatterjee080@gmail.com>
2026-06-13 18:56:39 +0530

    Initial deployment

---
+++ voice_of_the_doctor.py
@@ -0,0 +1,96 @@
+#step-1a
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+from gtts import gTTS
+
+def text_to_speech_with_gtts_old(input_text, output_filepath):
+    language = 'en'
+
+
+    audioobj= gTTS(
+        text=input_text, 
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+
+input_text="Hello, I am your AI doctor. How can I assist you today?"
+# text_to_speech_with_gtts_old(input_text=input_text, output_filepath="gtts_testing.mp3")
+
+#step-1b
+import elevenlabs
+from elevenlabs.client import ElevenLabs
+
+ELEVENLABS_API_KEY=os.environ.get("ELEVENLABS_API_KEY")
+
+def text_to_speech_with_elevenlabs_old(input_text,output_filepath): 
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+    audio=client.text_to_speech.convert(
+        text=input_text,
+        voice_id="EXAVITQu4vr4xnSDxMaL",
+        
+        model_id="eleven_turbo_v2",
+        output_format="mp3_44100_128"
+    )
+    elevenlabs.save(audio, output_filepath)
+
+# text_to_speech_with_elevenlabs_old(input_text=input_text, output_filepath="elevenlabs_testing.mp3")
+
+#step-2
+import subprocess
+import platform
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language = 'en'
+
+
+    audioobj= gTTS(
+        text=input_text, 
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name=platform.system()
+    try:
+        if os_name== "Darwin":
+            subprocess.run(["open", output_filepath])
+        elif os_name=="Windows":
+            subprocess.run(["powerShell", "-Command", f"Start-Process '{output_filepath}'"])
+        elif os_name=="Linux":
+            subprocess.run(["aplay", output_filepath])
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+input_text="Hello, I am your AI doctor.new version testing!"
+text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing_autoplay.mp3")
+
+def text_to_speech_with_elevenlabs(input_text,output_filepath): 
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+    audio=client.text_to_speech.convert(
+        text=input_text,
+        voice_id="EXAVITQu4vr4xnSDxMaL",
+        
+        model_id="eleven_turbo_v2",
+        output_format="mp3_44100_128"
+    )
+    elevenlabs.save(audio, output_filepath)
+    os_name=platform.system()
+    try:
+        if os_name== "Darwin":
+            subprocess.run(["open", output_filepath])
+        elif os_name=="Windows":
+            subprocess.run(["powerShell", "-Command", f"Start-Process '{output_filepath}'"])
+        elif os_name=="Linux":
+            subprocess.run(["aplay", output_filepath])
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+text_to_speech_with_elevenlabs(input_text=input_text, output_filepath="elevenlabs_testing_autoplay.mp3")
---
+++ voice_of_the_patient.py
@@ -0,0 +1,71 @@
+# setup audio recorder(ffmpeg & portaudio)
+import os
+import logging
+import speech_recognition as sr
+from pydub import AudioSegment
+from io import BytesIO
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def record_audio(file_path, timeout=20, phrase_time_linit=None):
+    """
+    Simplified function to record audio from the microphone and save it as an mp3 file.
+
+    Args:
+    file_path (str): path to save the recorded audio file (should end with .mp3)
+    timeout (int): maximum time to wait for a phrase to start (in seconds).
+    phrase_time_linit (int): maximum duration of a single phrase (in seconds).
+    """
+    recognizer = sr.Recognizer()
+    try:
+            with sr.Microphone() as source:
+
+                logging.info("Calibrating microphone...")
+                recognizer.adjust_for_ambient_noise(source, duration=2)
+
+                logging.info("Speak now...")
+
+                audio_data = recognizer.listen(
+                    source,
+                    timeout=20,
+                    phrase_time_limit=15
+                )
+
+                logging.info("Recording complete.")
+                #convert the audio data to an MP3 file
+                wav_data = audio_data.get_wav_data()
+                audio_segment = AudioSegment.from_wav(BytesIO(wav_data))
+                with open(file_path, "wb") as f:
+                    f.write(audio_data.get_wav_data())
+                logging.info(f"Audio saved to {file_path}")
+    except Exception as e:
+        logging.error(f"An error occurred while recording audio: {e}")
+
+audio_file_path = "patient_voice.wav" 
+
+record_audio(file_path=audio_file_path)
+
+#setup speech to text-STT-model for transcription
+from dotenv import load_dotenv
+import os
+load_dotenv()
+from groq import Groq
+
+GROQ_API_KEY =os.environ.get("GROQ_API_KEY")
+
+def transcribe_with_groq(stt_model, audio_file_path,GROQ_API_KEY):
+    client= Groq(api_key=GROQ_API_KEY)
+    stt_model="whisper-large-v3-turbo"
+    audio_file=open(audio_file_path, "rb")
+
+    transcription=client.audio.transcriptions.create(
+        model=stt_model,
+        file=audio_file,
+        language="en",
+    )
+
+    return transcription.text
+
+
+
+
